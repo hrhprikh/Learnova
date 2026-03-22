6c4c916868c760d050853b381ecb9bfb5e355020
Harsh Parikh <146793525+hrhprikh@users.noreply.github.com>
2026-03-22 08:43:58 +0530

    Update api.ts

--- apps/api/netlify/functions/api.ts
+++ apps/api/netlify/functions/api.ts
@@ -1,5 +1,5 @@
 import serverless from "serverless-http";
-import { app } from "../../src/app.js";
+import { app } from "../../src/app";
 
 // Netlify invokes functions under '/.netlify/functions/<name>',
 // so we strip that prefix before Express route matching.
